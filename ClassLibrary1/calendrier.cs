4edb0225711c4190262b2e918369b5d4e7bb0b1d
omardiall <omardiallo2005@gmail.com>
2025-06-06 16:58:31 +0200

    Update calendrier.cs

--- ClassLibrary1/calendrier.cs
+++ ClassLibrary1/calendrier.cs
@@ -1,8 +1,36 @@
 namespace space AnneesBissextiles{
 	
-	 public class CalendrierGregorien
-{ 
-	 
+	 public class calendriergregorien
+{
+	public static bool estbissextile(ushort annee) {
+
+
+		if (annee == 0) { throw new ArgumentOutOfRangeException("annee"); }
+
+
+
+		if (annee % 400 == 0)
+		{
+			return true;
+
+		}
+		else if (annee % 100 == 0)
+		{
+
+			return false;
+		}
+		else if (annee % 4 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+
+	}
+
 
 
 
